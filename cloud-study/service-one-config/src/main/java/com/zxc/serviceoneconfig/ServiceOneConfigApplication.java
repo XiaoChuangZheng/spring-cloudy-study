9c26dafa1be031febead6fe5840ce9f2872903d7
zhengxiaochuang <zhengxiaochuang@eefung.com>
2019-04-28 15:31:34 +0800

    refactor: 修改配置文件

--- cloud-study/service-one-config/src/main/java/com/zxc/serviceoneconfig/ServiceOneConfigApplication.java
+++ cloud-study/service-one-config/src/main/java/com/zxc/serviceoneconfig/ServiceOneConfigApplication.java
@@ -3,12 +3,14 @@ package com.zxc.serviceoneconfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
 @RestController
 public class ServiceOneConfigApplication {
 
