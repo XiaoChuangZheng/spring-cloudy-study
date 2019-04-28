bc2c908ff03c3fb4b54563243b3eebcc0c163d28
zhengxiaochuang <zhengxiaochuang@eefung.com>
2019-04-28 15:19:24 +0800

    feat: 增加配置服务

---
+++ cloud-study/service-one-config/src/main/java/com/zxc/serviceoneconfig/ServiceOneConfigApplication.java
@@ -0,0 +1,27 @@
+package com.zxc.serviceoneconfig;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ServiceOneConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceOneConfigApplication.class, args);
+    }
+
+    @Value("${service.one.config.message}")
+    private String name;
+
+    @RequestMapping(value = "/service-one", method = RequestMethod.GET)
+    public String serviceOneName() {
+        return name;
+    }
+
+}
